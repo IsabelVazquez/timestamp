e2c58525f0aa29dac08c253c0126761f11f804af
Isabel <12ivazquez@gmail.com>
2017-06-07 00:37:00 +0000

    [#2 pass query w/o ?]

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const app = express()
+const app = express();
+const moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+
+app.get('/:query', function (req, res) {
+  var string = req.params.query;
+  res.send(JSON.stringify({
+    "unix" : string,
+    "natural" : string
+  }));
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
