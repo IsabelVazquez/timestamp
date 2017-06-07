9907864c6b4cb527e8f81bc92d93c4b14244993c
Isabel <12ivazquez@gmail.com>
2017-06-07 16:48:14 +0000

    [#3/Isabel] 3 string scenarios are handled

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
 const moment = require('moment');
-
+var regex = new RegExp(/^[0-9]+$/);
 
 app.get('/:query', function (req, res) {
   var string = req.params.query;
-  res.send(JSON.stringify({
+
+  if (regex.test(string) === true) {
+    var time = moment.unix(string).format("MMMM DD, YYYY")
+    res.send(JSON.stringify({
     "unix" : string,
-    "natural" : string
-  }));
+    "natural" : time
+    }));
+  }
+  
+  if (moment(string, "MMMM DD, YYYY").isValid() === true) {
+    res.send(JSON.stringify({
+      "unix" : moment(string, "MMMM DD, YYYY").unix(),
+      "natural" : string
+    }));  
+  }
+  
+  else {
+    res.send(JSON.stringify({
+      "unix" : null,
+      "natural" : null
+    })); 
+  }
 });
 
 app.listen(8080, function () {
