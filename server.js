e92420c33c08abc9e2c389cdb61a4cd41342d724
Isabel <12ivazquez@gmail.com>
2017-06-08 19:11:54 +0000

    [#5/Isabel] All files w/ stories done

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
 const express = require('express');
 const app = express();
 const moment = require('moment');
-var regex = new RegExp(/^[0-9]+$/);
+//TBD: improve regex
+var regex = new RegExp(/([0-9]){10}/);
+
+//if there is no query, there should just be the homepage
+app.route('/').get(function(req, res) {
+      res.sendFile(process.cwd() + '/index.html');
+    });
+
 
 app.get('/:query', function (req, res) {
   var string = req.params.query;
-
+  
   if (regex.test(string) === true) {
     var time = moment.unix(string).format("MMMM DD, YYYY")
-    res.send(JSON.stringify({
+    return res.send(JSON.stringify({
     "unix" : string,
     "natural" : time
     }));
   }
   
   if (moment(string, "MMMM DD, YYYY").isValid() === true) {
-    res.send(JSON.stringify({
+    return res.send(JSON.stringify({
+      // TBD: improve unix
       "unix" : moment(string, "MMMM DD, YYYY").unix(),
       "natural" : string
-    }));  
+    }));
   }
   
   else {
-    res.send(JSON.stringify({
+    return res.send(JSON.stringify({
       "unix" : null,
       "natural" : null
-    })); 
+    }));
   }
 });
 
