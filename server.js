26dae44cf28b3b9299febb742c378e90167beb67
IsabelVazquez <IsabelVazquez@users.noreply.github.com>
2017-06-13 19:34:50 -0500

    Fixing some spacing issues

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var regex = new RegExp(/([0-9]){10}/);
 //if there is no query, there should just be the homepage
 app.route('/').get(function(req, res) {
       res.sendFile(process.cwd() + '/index.html');
-    });
+});
 
 
 app.get('/:query', function (req, res) {
@@ -18,8 +18,8 @@ app.get('/:query', function (req, res) {
   if (regex.test(string) === true) {
     var time = moment.unix(string).format("MMMM DD, YYYY")
     return res.send(JSON.stringify({
-    "unix" : string,
-    "natural" : time
+      "unix" : string,
+      "natural" : time
     }));
   }
   
